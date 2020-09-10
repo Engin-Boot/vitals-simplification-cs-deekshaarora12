98abaec79c2481adc7f27a5b57ceb6eb96c775ac
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-10 23:58:10 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,38 +1,3 @@
-/*using System;
-using System.Diagnostics;
-
-class Checker
-{
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
-        return true;
-    }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
-    static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
-        return 0;
-    }
-}*/
-
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -77,14 +42,13 @@ namespace Vitals_Simplification
              string name;
              float upperlimit;
              float lowerlimit;
-             float singlelimit;
 
-            internal vitals(string name, float upperlimit, float lowerlimit, float singlelimit)
+            internal vitals(string name, float upperlimit, float lowerlimit)
             {
                 this.name = name;
                 this.upperlimit = upperlimit;
                 this.lowerlimit = lowerlimit;
-                this.singlelimit = singlelimit;
+               
             }
 
 
@@ -98,7 +62,7 @@ namespace Vitals_Simplification
             }
             public bool checkNormal(string vitalname, float value)
             {
-                return ((value >= lowerlimit && value <= upperlimit)||(value == singlelimit));
+                return ((value >= lowerlimit && value <= upperlimit));
             }
 
             public void CheckVitals(Alert alert,string vitalname,float value)
@@ -116,14 +80,67 @@ namespace Vitals_Simplification
                 alert.GetAlert(vitalname,vitalalertinfo);
             }
         }
+        static void ExpectTrue(bool expression)
+        {
+            if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                Environment.Exit(1);
+            }
+        }
+        static void ExpectFalse(bool expression)
+        {
+            if (expression)
+            {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+            }
+        }
         static void Main()
         {
             AlertInSMS alertsms = new AlertInSMS();
             AlertInSound alertsound = new AlertInSound();
 
-            vitals vt = new vitals("Bpm", 150, 70, 0);
-            vt.CheckVitals(alertsms, "Bpm", 40);
-            vt.CheckVitals(alertsound, "Bpm", 170);
+            vitals bpm = new vitals("Bpm", 150, 70);
+            bpm.CheckVitals(alertsms, "Bpm", 40);
+            bpm.CheckVitals(alertsound, "Bpm", 170);
+            bpm.CheckVitals(alertsound, "Bpm", 150);
+
+            ExpectTrue(bpm.checkHigh("Bpm",190));
+            ExpectTrue(bpm.checkLow("Bpm", 19));
+            ExpectTrue(bpm.checkNormal("Bpm", 70));
+
+            ExpectFalse(bpm.checkHigh("Bpm", 17));
+            ExpectFalse(bpm.checkLow("Bpm", 70));
+            ExpectFalse(bpm.checkNormal("Bpm", 120));
+
+            vitals resp = new vitals("Resp", 95, 30);
+            resp.CheckVitals(alertsms, "Resp", 14);
+            resp.CheckVitals(alertsound, "Resp", 170);
+            resp.CheckVitals(alertsound, "Resp", 30);
+
+            ExpectTrue(resp.checkHigh("Resp", 96));
+            ExpectTrue(resp.checkLow("Resp", 29));
+            ExpectTrue(resp.checkNormal("Resp", 30));
+
+            ExpectFalse(resp.checkHigh("Resp", 17));
+            ExpectFalse(resp.checkLow("Resp", 30));
+            ExpectFalse(resp.checkNormal("Resp", 120));
+
+            vitals spo2 = new vitals("Spo2", 90, 90);
+            spo2.CheckVitals(alertsms, "Spo2", 14);
+            spo2.CheckVitals(alertsound, "Spo2", 120);
+            spo2.CheckVitals(alertsound, "Spo2", 90);
+
+            ExpectTrue(spo2.checkHigh("Spo2", 109));
+            ExpectTrue(spo2.checkLow("Spo2", 45));
+            ExpectTrue(spo2.checkNormal("Spo2", 90));
+
+            ExpectFalse(spo2.checkHigh("Spo2", 17));
+            ExpectFalse(spo2.checkLow("Spo2", 90));
+            ExpectFalse(spo2.checkNormal("Spo2", 120));
+
+
 
         }
     }
